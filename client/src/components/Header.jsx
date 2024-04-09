63275292088165b97ddda3ea8d0218276f29eef7
Mitch Barry <mitchbarry5564@gmail.com>
2024-04-09 16:03:13 -0500

    Bug Fixes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,43 +23,45 @@ const Header = () => {
 			<img src={favicon} className="favicon-img" alt="Pokeball Favicon"/>
 			<Navbar className="navbar-container border border-warning border-3 rounded-5">
 				<Nav className="nav-container">
-					<Nav.Item className="nav-item-20 text-warning">
+					<Nav.Item className="nav-item-img text-warning">
 						<Link to={authToken ? "/lobbies/home" : location.pathname === "/register" ? "/register" : "/login"}>
 							<img src={pokeChess} className="pokeChess-img" alt="PokeChess"/>
 						</Link>
 					</Nav.Item>
-					<Nav.Item className="nav-item text-warning">
-						<Link to={authToken ? "/lobbies/new" : location.pathname === "/register" ? "/register" : "/login"} className="link text-warning gap-1 d-flex flex-row">
-							<img src={multiplayerIcon} className="link-icon" alt="Multiplayer Icon"/>
-							Create Lobby
-						</Link>
-					</Nav.Item>
-					<Nav.Item className="nav-item text-warning">
-						<Link to="/pokedex" className="link text-warning gap-1 d-flex flex-row">
-							<img src={pokedexIcon} className="link-icon" alt="Pokedéx Icon"/>
-							Pokédex
-						</Link>
-					</Nav.Item>
-					<Nav.Item className="nav-item text-warning">
-						<Link to="/pokenews" className="link text-warning gap-1 d-flex flex-row">
-							PokéNews
-							<img src={newsIcon} className="link-icon" alt="PokéNews Icon"/>
-						</Link>
-					</Nav.Item>
-					<Nav.Item className="nav-item text-warning">
+					<div className="flexBoxSpaceBetween">
+						<Nav.Item className="nav-item text-warning">
+							<Link to={authToken ? "/lobbies/new" : location.pathname === "/register" ? "/register" : "/login"} className="link text-warning gap-1 d-flex flex-row">
+								<img src={multiplayerIcon} className="link-icon" alt="Multiplayer Icon"/>
+								Create Lobby
+							</Link>
+						</Nav.Item>
+						<Nav.Item className="nav-item text-warning">
+							<Link to="/pokedex" className="link text-warning gap-1 d-flex flex-row">
+								<img src={pokedexIcon} className="link-icon" alt="Pokedéx Icon"/>
+								Pokédex
+							</Link>
+						</Nav.Item>
+						<Nav.Item className="nav-item text-warning">
+							<Link to="/pokenews" className="link text-warning gap-1 d-flex flex-row">
+								PokéNews
+								<img src={newsIcon} className="link-icon" alt="PokéNews Icon"/>
+							</Link>
+						</Nav.Item>
+					</div>
+					<Nav.Item className="nav-item-button text-warning">
 						{authToken ? (
-							<Button variant="outline-warning" onClick={handleLogout}>
+							<Button variant="outline-warning" onClick={handleLogout} className="item-button">
 								Logout
 							</Button>) : (
 							location.pathname === "/login" ? (
 								<Link to="/register">
-									<Button variant="outline-warning">
+									<Button variant="outline-warning" className="item-button">
 										Register
 									</Button>
 								</Link>
 							) : (
 								<Link to="/login">
-									<Button variant="outline-warning">
+									<Button variant="outline-warning" className="item-button">
 										Login
 									</Button>
 								</Link>
