f1919022e96a82f847c2c27f57252cfc3e2d9a6c
Mitch Barry <mitchbarry5564@gmail.com>
2024-06-18 23:28:18 -0500

    Semi-colons added

--- server/server.js
+++ server/server.js
@@ -1,51 +1,51 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-import { Server } from 'socket.io'
+import { Server } from 'socket.io';
 
-import dbConnect from './src/config/mongoose.config.js'
+import dbConnect from './src/config/mongoose.config.js';
 
-import userRouter from './src/routes/user.routes.js'
-import authRouter from './src/routes/auth.routes.js'
-import lobbyRouter from './src/routes/lobby.routes.js'
-import pokemonRouter from './src/routes/pokemon.routes.js'
+import userRouter from './src/routes/user.routes.js';
+import authRouter from './src/routes/auth.routes.js';
+import lobbyRouter from './src/routes/lobby.routes.js';
+import pokemonRouter from './src/routes/pokemon.routes.js';
 
-import errorUtilities from './src/utilities/error.utilities.js'
-import sockets from './src/sockets/socket.js'
+import errorUtilities from './src/utilities/error.utilities.js';
+import sockets from './src/sockets/socket.js';
 
-dbConnect()
+dbConnect();
 
-const app = express()
+const app = express();
 
-app.use(express.json(),express.urlencoded({extended:true}))
-app.use(cors({credentials: true, origin: 'http://localhost:5173'}))
-dotenv.config()
+app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+dotenv.config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use('/api/users', userRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/lobbies', lobbyRouter)
-app.use('/api/pokemon', pokemonRouter)
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/lobbies', lobbyRouter);
+app.use('/api/pokemon', pokemonRouter);
 
 app.use((req, res, next) => {
-    const error = new Error('Not Found')
-    error.statusCode = 404
-    error.name = 'Not Found'
-    next(error)
-})
+    const error = new Error('Not Found');
+    error.statusCode = 404;
+    error.name = 'Not Found';
+    next(error);
+});
 
 app.use((error, req, res, next) => {
-    error.name === 'ValidationError' ? error.statusCode = 400 : ''
-    const normalizedError = errorUtilities.normalizeError(error)
-    res.status(normalizedError.statusCode).json(normalizedError)
-})
+    error.name === 'ValidationError' ? error.statusCode = 400 : '';
+    const normalizedError = errorUtilities.normalizeError(error);
+    res.status(normalizedError.statusCode).json(normalizedError);
+});
 
 const server = app.listen(PORT, () =>
     console.log(`Listening on port: ${PORT}`)
-)
+);
 
-const io = new Server(server, {cors: true}) // Initialize the socket instance with a new server
+const io = new Server(server, { cors: true }); // Initialize the socket instance with a new server
 
-sockets.listen(io)
+sockets.listen(io);
--- server/src/utilities/error.utilities.js
+++ server/src/utilities/error.utilities.js
@@ -1,26 +1,26 @@
 const errorUtilities = {
     normalizeError(error) {
         const normalizedError = {
-            statusCode: error.statusCode || error.name === 'ValidationError' ? 400 : 500,
+            statusCode: error.statusCode || (error.name === 'ValidationError' ? 400 : 500),
             message: error.message || 'Something went wrong.',
             name: error.name || 'Server Error.',
             validationErrors: this.extractValidationErrors(error)
-        }
-        return normalizedError
+        };
+        return normalizedError;
     },
 
     extractValidationErrors(error) {
-        const validationErrors = {}
+        const validationErrors = {};
         if (error.name === 'ValidationError') {
             for (const field in error.errors) {
                 if (error.errors.hasOwnProperty(field)) {
-                    const errorMessage = error.errors[field].message
-                    validationErrors[field] = errorMessage
+                    const errorMessage = error.errors[field].message;
+                    validationErrors[field] = errorMessage;
                 }
             }
         }
-        return validationErrors
+        return validationErrors;
     }
-}
+};
 
-export default errorUtilities
+export default errorUtilities;
